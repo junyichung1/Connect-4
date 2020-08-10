07ca5ea1cc6e098119c0714e7fc2ac2888575250
Junyi Chung <junyichung@junyis-air.home>
2020-08-09 19:32:34 -0700

    fill in array correctly

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
 /*----- constants -----*/
 const PLAYERS = {
-    player1: 1,
-    player2: -1
-}
+  player1: 1,
+  player2: -1,
+};
 
 const MAXGUESS = 42;
 // const WINCOMBOS = ??
-
 
 /*----- app's state (variables) -----*/
 let turn;
@@ -14,50 +13,68 @@ let winner;
 let board;
 
 /*----- cached element references -----*/
-const squareEl = document.querySelectorAll("tr td");
+const rowEl = document.querySelectorAll("tr");
 const messageEl = document.getElementById("msg");
 
 /*----- event listeners -----*/
-document.querySelector("table").addEventListener('click', handleDrop);
-document.querySelector("button").addEventListener('click', renderMsg);
+document.querySelector("table").addEventListener("click", handleDrop);
+// document.querySelector("button").addEventListener("click", renderMsg);
 
 /*----- functions -----*/
 init();
 
-function init() {
-// reset all variables
-    turn = 1;
-    winner = null;
-    // each array inside the array is a column
-    // board = [
-    //     [null, null, null, null, null, null,],
-    //     [null, null, null, null, null, null,],
-    //     [null, null, null, null, null, null,],
-    //     [null, null, null, null, null, null,],
-    //     [null, null, null, null, null, null,],
-    //     [null, null, null, null, null, null,],
-    //     [null, null, null, null, null, null,]
-    // ]
-    board = new Array(7).fill([null, null, null, null, null, null])
-    render();
-}
-
-function render() {
-    
-}
-
 function handleDrop(evt) {
-    let squareIdx = parseInt(evt.target.id.substr(3,3));
-    let rowIdx = parseInt(evt.target.className.replace("c", ""));
+  let colIdx = evt.target.className.replace("c", "");
+  for (let i = 0; i < 6; i++) {
+    if (board[colIdx][i] === null) {
+      board[colIdx][i] = turn;
+      return;
+    }
     turn *= -1;
-    // need to update board in this function
-    checkWinner();
+    // input where to put in array of array
+  }
+  render(evt.target);
+  //     // need to update board in this function
+  //   //   checkWinner();
 }
 
-function renderMsg() {
-
+function init() {
+  // reset all variables
+  turn = 1;
+  winner = null;
+  // each array inside the array is a column
+  board = [
+    [null, null, null, null, null, null],
+    [null, null, null, null, null, null],
+    [null, null, null, null, null, null],
+    [null, null, null, null, null, null],
+    [null, null, null, null, null, null],
+    [null, null, null, null, null, null],
+    [null, null, null, null, null, null],
+  ];
 }
 
-function getWinner() {
-
+function render(elem) {
+  //   change color of board
+  let row = [];
+  let colIdx = elem.className.replace("c", "");
+  for (let i = 5; i > -1; i--) {
+    if (rowEl[i].children[colIdx].style.backgroundColor === "black") {
+        row.push(rowEl[i].children[colIdx])
+        if (turn >0) {
+            row[0].style.backgroundColor = "red";
+        } else {
+            row[0].style.backgroundColor = "yellow";
+        }
+    //   rowEl[i].children[colIdx].style.backgroundColor =
+    //     turn > 0 ? "red" : "yellow";
+      
+    }
+  }
 }
+
+// function renderMsg() {}
+
+// function getWinner() {
+
+// }
